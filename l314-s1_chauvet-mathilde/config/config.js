f9738c3fdc560eed4c5af99372ff1a2719460c8f
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-12 22:46:21 +0100

    Suppression doublon config pour suppression tip

--- l314-s1_chauvet-mathilde/config/config.js
+++ l314-s1_chauvet-mathilde/config/config.js
@@ -1,19 +1,16 @@
-require('dotenv').config();
-
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": 'mysql'
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": 'mysql'
-  }
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
 };
-
