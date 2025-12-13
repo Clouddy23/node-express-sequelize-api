63c468e68ac8fcef1ca7d499d742ecb9f2e557a6
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-13 18:47:28 +0100

    ajout fonction route récupération users

--- l314-s1_chauvet-mathilde/routes/route.js
+++ l314-s1_chauvet-mathilde/routes/route.js
@@ -5,4 +5,7 @@ const controller = require("../controllers/controller");
 // define the home page route
 router.get("/", controller.helloworld);
 
+// Ajout fonction pour récupérer users
+router.get("users", controller.getUsers);
+
 module.exports = router;
