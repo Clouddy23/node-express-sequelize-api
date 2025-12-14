6ed9ac3345c6a13441a7c3e78051b637019e4b09
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-14 15:37:35 +0100

    Modification commentaire

--- l314-s1_chauvet-mathilde/routes/route.js
+++ l314-s1_chauvet-mathilde/routes/route.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
 
+//------------------------------ CRUD Users ------------------------------
+
 // Page home
 router.get("/", controller.helloworld);
 
-// CRUD Users
 // Ajout de la fonction de récupération de tous les users
 router.get("/users", controller.getUsers);
 
