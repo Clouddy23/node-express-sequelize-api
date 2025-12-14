0b1c0befc98ff3c2a1e92af88bd8eea1b6594161
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-14 15:23:53 +0100

    ajout fonction création d'un user

--- l314-s1_chauvet-mathilde/routes/route.js
+++ l314-s1_chauvet-mathilde/routes/route.js
@@ -12,4 +12,7 @@ router.get("/users", controller.getUsers);
 //Ajourt de la fonction de récupération d'un seul user via son id
 router.get("/users/:id", controller.getUserById);
 
+// Ajout de la fonction création d'un user
+router.post("/users", controller.createUser);
+
 module.exports = router;
