493f11330e735007d58086df0e88dc9ecaef1573
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-14 13:37:29 +0100

    Création du fichier de test, appel supertest

---
+++ l314-s1_chauvet-mathilde/tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const express = require("../app");
+
+describe("Integration Tests for API Endpoints", () => {
+  it("should respond with Hello World", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("Hello controller!");
+  });
+});
