6f8212da2bb4724e00711b647c415fcbd0bfb053
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-14 15:16:29 +0100

    suppresion doublon server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config({ quiet: true });
-const app = require("./app");
-
-const port = process.env.PORT;
-
-// Démarrer le serveur
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
