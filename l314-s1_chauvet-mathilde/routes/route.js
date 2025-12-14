3841d9cc9e9e2f219198454e0249c17100df07f1
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-14 15:23:14 +0100

    Ajout fonction récupération 1 user via ID

--- l314-s1_chauvet-mathilde/routes/route.js
+++ l314-s1_chauvet-mathilde/routes/route.js
@@ -9,4 +9,7 @@ router.get("/", controller.helloworld);
 // Ajout de la fonction de récupération de tous les users
 router.get("/users", controller.getUsers);
 
+//Ajourt de la fonction de récupération d'un seul user via son id
+router.get("/users/:id", controller.getUserById);
+
 module.exports = router;
