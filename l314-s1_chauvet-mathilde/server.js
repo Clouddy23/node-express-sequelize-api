e0865e20ba6f5602ef9165ac8bf64ebccc6a0cda
mathildechauvet <mathildechauvet@outlook.fr>
2025-12-14 17:07:27 +0100

    Import modèles

--- l314-s1_chauvet-mathilde/server.js
+++ l314-s1_chauvet-mathilde/server.js
@@ -1,6 +1,9 @@
 require("dotenv").config({ quiet: true });
 const app = require("./app");
 
+// Importation des modèles pour connexion à la BDD
+const db = require("./models");
+
 const port = process.env.PORT || 3000;
 
 // Démarrer le serveur
